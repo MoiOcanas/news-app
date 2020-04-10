28f4a1ce9e7a3b66afc07a75ae56c56a6d3d1228
MoiOcanas <aocanas@webforcehq.com>
2020-04-10 15:49:00 -0500

    change on fbconfig file

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -5,13 +5,13 @@ import 'firebase/storage';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDwoqMxXxhrrRMn1tno-_BWdYKXyN6d5Iw",
-    authDomain: "gallery-fire.firebaseapp.com",
-    databaseURL: "https://gallery-fire.firebaseio.com",
-    projectId: "gallery-fire",
-    storageBucket: "gallery-fire.appspot.com",
-    messagingSenderId: "171311619988",
-    appId: "1:171311619988:web:c6a4640025d678ad517981"
+    apiKey: "YOUR API KEY",
+    authDomain: "YOUR AUTHDOMAIN",
+    databaseURL: "YOUR DATABASE URL",
+    projectId: "YOUR PROJECT ID",
+    storageBucket: "YOUR STORAGEBUCKET",
+    messagingSenderId: "YOUR MESSAGING SERNDER ID",
+    appId: "YOUR APP ID"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
